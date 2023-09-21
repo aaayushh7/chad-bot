358123cd5629ad7cd01275860dab4940e6797272
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-09-21 12:54:57 +0530

    Initial commit

---
+++ components/main.js
@@ -0,0 +1,10 @@
+import Ask from './ask'
+
+export default () => {
+    return(
+        <main className="container mx-auto w-3/5 py-5">
+            <Ask/>
+            {/*Response*/}
+        </main>
+    )
+}
